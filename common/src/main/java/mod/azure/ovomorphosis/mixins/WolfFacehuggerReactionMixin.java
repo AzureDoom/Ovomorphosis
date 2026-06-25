becdc7966647af14948662a52dcbb35d9b6e54a7
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2026-06-25 09:09:52 -0400

    Cats/Wolfs now hiss/bark at alien mobs.

--- common/src/main/java/mod/azure/ovomorphosis/mixins/WolfFacehuggerReactionMixin.java
+++ common/src/main/java/mod/azure/ovomorphosis/mixins/WolfFacehuggerReactionMixin.java
@@ -1,7 +1,5 @@
 package mod.azure.ovomorphosis.mixins;
 
-import mod.azure.ovomorphosis.ai.util.TargetingUtils;
-import mod.azure.ovomorphosis.entities.AbstractAlienEntity;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.animal.Wolf;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,6 +7,9 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import mod.azure.ovomorphosis.ai.util.TargetingUtils;
+import mod.azure.ovomorphosis.entities.AbstractAlienEntity;
 
 @Mixin(Wolf.class)
 public class WolfFacehuggerReactionMixin {
