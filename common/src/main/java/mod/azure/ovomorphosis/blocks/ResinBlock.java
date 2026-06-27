3f30a5fbc78c70dcfceb077d0af7d4e1bc62e26e
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2026-06-27 13:32:06 -0400

    Don't spread if on fire or towards fire

--- common/src/main/java/mod/azure/ovomorphosis/blocks/ResinBlock.java
+++ common/src/main/java/mod/azure/ovomorphosis/blocks/ResinBlock.java
@@ -185,6 +185,9 @@ public class ResinBlock extends AbstractResinBlock {
         ) {
             return;
         }
+        if (level.getBlockState(pos.above()).is(Blocks.FIRE))
+            return;
+
         var directions = Direction.values();
         var start = random.nextInt(directions.length);
         for (var i = 0; i < directions.length; i++) {
@@ -197,6 +200,9 @@ public class ResinBlock extends AbstractResinBlock {
             var neighborState = level.getBlockState(neighbor);
 
             if (neighborState.is(ModTags.RESIN) || neighborState.is(ModTags.ACID_RESISTANT_BLOCKS))
+                continue;
+
+            if (neighborState.is(Blocks.FIRE) || level.getBlockState(neighbor.above()).is(Blocks.FIRE))
                 continue;
 
             if (!neighborState.getFluidState().isEmpty())
