8447d3b963705b4c543e800a8ea732a392856f04
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2026-07-02 16:29:23 -0400

    Fixes ovomorphs pushing back against other

--- common/src/main/java/mod/azure/ovomorphosis/entities/ovomorph/OvomorphEntity.java
+++ common/src/main/java/mod/azure/ovomorphosis/entities/ovomorph/OvomorphEntity.java
@@ -17,6 +17,7 @@ import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -105,7 +106,7 @@ public class OvomorphEntity extends AbstractAlienEntity {
             .add(Attributes.ARMOR_TOUGHNESS, CommonMod.getConfig().entityConfigs.ovomorphConfigs.ovomorphArmorToughness)
             .add(
                 Attributes.KNOCKBACK_RESISTANCE,
-                CommonMod.getConfig().entityConfigs.ovomorphConfigs.ovomorphKnockbackRes
+                1.0
             )
             .add(Attributes.FOLLOW_RANGE, 0.0)
             .add(Attributes.MOVEMENT_SPEED, 0.0);
@@ -128,9 +129,14 @@ public class OvomorphEntity extends AbstractAlienEntity {
 
     @Override
     public void doPush(@NotNull Entity entity) {
+        if (entity instanceof OvomorphEntity) {
+            return;
+        }
+
         if (
-            !level().isClientSide && (entity instanceof LivingEntity living &&
-                TargetingUtils.faceHuggerTest(this, living))
+            !level().isClientSide &&
+                entity instanceof LivingEntity living &&
+                TargetingUtils.faceHuggerTest(this, living)
         ) {
             this.setEggState(EggStates.HATCHING.ordinal());
         }
@@ -190,6 +196,8 @@ public class OvomorphEntity extends AbstractAlienEntity {
         if (tickCount == 1) {
             moveTo(blockPosition().offset(0, 0, 0), getYRot(), getXRot());
         }
+        this.setDeltaMovement(Vec3.ZERO);
+        this.hasImpulse = false;
 
         var yaw = 90.0f;
         this.setYRot(yaw);
