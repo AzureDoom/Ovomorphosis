d86351275e22300c91294ef890ca511a34c06eba
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2026-07-25 03:31:35 -0400

    Clean up

--- common/src/main/java/mod/azure/ovomorphosis/loot/OvomorphosisLootTables.java
+++ common/src/main/java/mod/azure/ovomorphosis/loot/OvomorphosisLootTables.java
@@ -9,26 +9,30 @@ public final class OvomorphosisLootTables {
     private OvomorphosisLootTables() {}
 
     public static final Set<ResourceLocation> SURVIVOR_NOTE_TARGETS = Set.of(
-        ResourceLocation.fromNamespaceAndPath("minecraft", "chests/abandoned_mineshaft"),
-        ResourceLocation.fromNamespaceAndPath("minecraft", "chests/simple_dungeon"),
-        ResourceLocation.fromNamespaceAndPath("minecraft", "chests/desert_pyramid"),
-        ResourceLocation.fromNamespaceAndPath("minecraft", "chests/ancient_city"),
-        ResourceLocation.fromNamespaceAndPath("minecraft", "chests/buried_treasure"),
-        ResourceLocation.fromNamespaceAndPath("minecraft", "chests/jungle_temple"),
-        ResourceLocation.fromNamespaceAndPath("minecraft", "chests/jungle_temple_dispenser"),
-        ResourceLocation.fromNamespaceAndPath("minecraft", "chests/mansion"),
-        ResourceLocation.fromNamespaceAndPath("minecraft", "chests/pillager_outpost"),
-        ResourceLocation.fromNamespaceAndPath("minecraft", "chests/shipwreck_supply"),
-        ResourceLocation.fromNamespaceAndPath("minecraft", "chests/shipwreck_treasure"),
-        ResourceLocation.fromNamespaceAndPath("minecraft", "chests/underwater_ruin_small"),
-        ResourceLocation.fromNamespaceAndPath("minecraft", "chests/underwater_ruin_big"),
-        ResourceLocation.fromNamespaceAndPath("minecraft", "chests/stronghold_corridor"),
-        ResourceLocation.fromNamespaceAndPath("minecraft", "chests/stronghold_crossing"),
-        ResourceLocation.fromNamespaceAndPath("minecraft", "chests/stronghold_library"),
-        ResourceLocation.fromNamespaceAndPath("minecraft", "chests/spawn_bonus_chest")
+        vanillaNamespace("chests/abandoned_mineshaft"),
+        vanillaNamespace("chests/simple_dungeon"),
+        vanillaNamespace("chests/desert_pyramid"),
+        vanillaNamespace("chests/ancient_city"),
+        vanillaNamespace("chests/buried_treasure"),
+        vanillaNamespace("chests/jungle_temple"),
+        vanillaNamespace("chests/jungle_temple_dispenser"),
+        vanillaNamespace("chests/mansion"),
+        vanillaNamespace("chests/pillager_outpost"),
+        vanillaNamespace("chests/shipwreck_supply"),
+        vanillaNamespace("chests/shipwreck_treasure"),
+        vanillaNamespace("chests/underwater_ruin_small"),
+        vanillaNamespace("chests/underwater_ruin_big"),
+        vanillaNamespace("chests/stronghold_corridor"),
+        vanillaNamespace("chests/stronghold_crossing"),
+        vanillaNamespace("chests/stronghold_library"),
+        vanillaNamespace("chests/spawn_bonus_chest")
     );
 
     public static boolean shouldInjectSurvivorNote(ResourceLocation id) {
         return SURVIVOR_NOTE_TARGETS.contains(id);
+    }
+
+    private static ResourceLocation vanillaNamespace(String path) {
+        return ResourceLocation.fromNamespaceAndPath("minecraft", path);
     }
 }
